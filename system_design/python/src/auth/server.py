7aa947c49e53df264329c5e1d20dc82fd0454647
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-09-14 18:03:36 -0400

    auth sever file updated with download code and minor fixes in manifests

--- system_design/python/src/auth/server.py
+++ system_design/python/src/auth/server.py
@@ -10,63 +10,84 @@ server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
 server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
 server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
-server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+server.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT"))
 # print(server.config["MYSQL_HOST"]) # to get the set values, just print it.
+print("MYSQL_HOST:", os.environ.get("MYSQL_HOST"))
+print("MYSQL_USER:", os.environ.get("MYSQL_USER"))
+print("MYSQL_PASSWORD:", os.environ.get("MYSQL_PASSWORD"))
+print("MYSQL_DB:", os.environ.get("MYSQL_DB"))
+print("MYSQL_PORT:", os.environ.get("MYSQL_PORT"))
+
 
 # create route
 @server.route("/login", methods=["POST"])
 def login():
+    """Handles login functionality.
+    Checks if user exists in the DB and returns a JWT.
+    """
+
     auth = request.authorization
-    # auth.username
-    # auth.password
     if not auth:
-        return "missing credentials", 401
-    
-    # check db for username and password
-    cur = mysql.connection.cursor()
-    res = cur.execute(
-        "SELECT email, password FROM user WHERE email=%s", (auth.username)
+        return "Missing credentials", 401
+
+    # Check DB for user
+    cursor = mysql.connection.cursor()
+    result = cursor.execute(
+        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
     )
 
-    if res > 0:
-        # user exists in our DB
-        user_row = cur.fetchone()
+    # Check result list to generate JWT or deny access
+    if result > 0:
+        user_row = cursor.fetchone()
         email = user_row[0]
         password = user_row[1]
 
-        # check if username and password in DB is same as in Request
         if auth.username != email or auth.password != password:
-            return "Invalid credentials!", 401
+            return "Invalid credentials", 401
         else:
             return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
-        
+
     else:
-        # user doesn't exist in our DB
-        return "Invalid credentials!", 401
+        return "Wrong username or password", 401
 
 @server.route("/validate", methods=["POST"])
 def validate():
+    """Validates a JWT within a request."""
+
     encoded_jwt = request.headers["Authorization"]
+
     if not encoded_jwt:
-        return "Missing Credentials",401
-    
+        return "Missing credentials", 401
+
     encoded_jwt = encoded_jwt.split(" ")[1]
+
     try:
         decoded = jwt.decode(
-            encoded_jwt, os.environ.get("JWT_SECRET"), algorithm=["HS256"]
+            encoded_jwt,
+            os.environ.get("JWT_SECRET"),
+            algorithms=["HS256"],
         )
+        print(decoded)
+        return decoded, 200
+    except jwt.ExpiredSignatureError:
+        return "JWT has expired", 401
+    except jwt.InvalidTokenError:
+        return f"Invalid JWT: {encoded_jwt}", 401
     except:
-        return "Not Authorized",403
+        return "Unauthorized", 403
     
-    return decoded,200
 
-def createJWT(username, secret, authz):
+def createJWT(username, secret, is_admin):
+    """Creates a JWT.
+    Default expiration is 24 hrs.
+    """
+
     return jwt.encode(
         {
-            "username":username,
-            "exp":datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1),
-            "iat":datetime.datetime.utcnow(),
-            "admin":authz
+            "username": username,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": is_admin,
         },
         secret,
         algorithm="HS256",
@@ -74,7 +95,7 @@ def createJWT(username, secret, authz):
 
 if __name__ == "__main__":
     # print(__name__)
-    server.run(host="0.0.0.0",port=5000,) # this tells your OS to listen on all public IP's
+    server.run(host="0.0.0.0",port=5000,debug=True) # this tells your OS to listen on all public IP's
 
     """
     The host is the server that is hosting our application.
